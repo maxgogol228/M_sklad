82e66f30db50c46795a2f1548b686822a19a6348
maxgogol228 <maxgogolonline@gmail.com>
2026-04-08 20:37:22 +0300

    Update settings.py

--- msklad/settings.py
+++ msklad/settings.py
@@ -71,8 +71,9 @@ WSGI_APPLICATION = 'msklad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', # или postgresql
         'NAME': BASE_DIR / 'db.sqlite3',
+        # ... остальные настройки для PostgreSQL
     }
 }
 
