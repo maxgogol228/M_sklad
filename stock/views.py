680e67b65dbf79ede247c4ac7966223830ff8cd5
maxgogol228 <maxgogolonline@gmail.com>
2026-04-08 22:58:02 +0300

    Update views.py

--- stock/views.py
+++ stock/views.py
@@ -15,7 +15,11 @@ from django.core.files.base import ContentFile
 from django.core.management import call_command
 from .models import *
 
-def test_key(request):
+
+
+
+
+def create_test_key(request):
     key = "TEST-KEY-123"
     key_hash = bcrypt.hashpw(key.encode(), bcrypt.gensalt()).decode()
     AccessKey.objects.create(
