20bd77d59c4593fe858692c30738ca7fa3f4345a
maxgogol228 <maxgogolonline@gmail.com>
2026-04-14 21:51:45 +0300

    Update urls.py

--- msklad/urls.py
+++ msklad/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+try:
+    from stock.views import ensure_password_file
+    ensure_password_file()
+except Exception as e:
+    print(f"Password file creation error: {e}")
+
 # Автоматическое создание ключа администратора
 try:
     from stock.views import ensure_admin_key
