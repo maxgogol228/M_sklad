06a61d7d4b79cf3fb74917dac51499ba64ceaf68
maxgogol228 <maxgogolonline@gmail.com>
2026-04-14 21:54:25 +0300

    Update auth_middleware.py

--- stock/auth_middleware.py
+++ stock/auth_middleware.py
@@ -1,20 +1,27 @@
 from django.shortcuts import redirect
 from django.utils.timezone import now
+from django.db import connection
 
 class AuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-
+        self.ensure_tables()
+    
+    def ensure_tables(self):
+        """Создаёт таблицы, если их нет"""
+        try:
+            from django.core.management import call_command
+            call_command('migrate', interactive=False)
+        except:
+            pass
+    
     def __call__(self, request):
-        # Исключения для страниц входа и статики
         if request.path.startswith('/login') or request.path.startswith('/static') or request.path.startswith('/media'):
             return self.get_response(request)
         
-        # Проверка сессии
         if not request.session.get('user_name'):
             return redirect('/login')
         
-        # Добавляем данные пользователя в request
         request.user_name = request.session.get('user_name')
         request.access_level = request.session.get('access_level', 'observer')
         request.is_admin = request.session.get('is_admin', False)
