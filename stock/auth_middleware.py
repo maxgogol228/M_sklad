ecde403ffc9c958ddad832d76aa1a3b92a50482e
maxgogol228 <maxgogolonline@gmail.com>
2026-04-15 15:29:15 +0300

    Update auth_middleware.py

--- stock/auth_middleware.py
+++ stock/auth_middleware.py
@@ -5,15 +5,12 @@ class AuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Исключения для страниц входа и статики
         if request.path.startswith('/login') or request.path.startswith('/static') or request.path.startswith('/media'):
             return self.get_response(request)
         
-        # Проверка сессии
         if not request.session.get('user_name'):
             return redirect('/login')
         
-        # Добавляем данные в request
         request.user_name = request.session.get('user_name')
         request.access_level = request.session.get('access_level', 'observer')
         request.is_admin = request.session.get('is_admin', False)
