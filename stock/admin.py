aca9923f4db70f2d92b7b0e0b8c4dffa4c871ace
maxgogol228 <maxgogolonline@gmail.com>
2026-04-20 20:42:47 +0300

    Update admin.py

--- stock/admin.py
+++ stock/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import AccessKey, UserSession, Part, Device, DevicePart, Order, Log
+from .models import Part, Device, DevicePart, Order
 
-admin.site.register(AccessKey)
-admin.site.register(UserSession)
 admin.site.register(Part)
 admin.site.register(Device)
 admin.site.register(DevicePart)
 admin.site.register(Order)
-admin.site.register(Log)
